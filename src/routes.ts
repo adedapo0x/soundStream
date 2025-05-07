ee056857b641878152cd05557e40d9e23d3bada0
Adedapo Adeyemi <adedapo025@gmail.com>
2025-05-07 16:51:21 +0100

    implementing callback route after auth

--- src/routes.ts
+++ src/routes.ts
@@ -13,4 +13,25 @@ router.get('/login', (req: Request, res: Response) => {
     res.redirect(spotifyAPI.createAuthorizeURL(scopes, state));
 });
 
+router.get('/callback', (req: Request, res: Response) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+
+    if (error){
+        throw new Error(`Error occured: ${error}`);
+    }
+
+    if (state === null){
+        res.redirect('/#error=state_mismatch'); 
+        return
+    }
+
+    if (typeof code === "string"){
+        spotifyAPI.authorizationCodeGrant(code).then(data => {
+            
+        })
+    }
+})
+
 
